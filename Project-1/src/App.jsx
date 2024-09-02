4fd25ab8e24c4eae3a8872ff108f4f16a13d2fe2
priya-patel-13 <ppiyu4487@gmail.com>
2024-09-02 16:23:28 +0530

    hooks

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import Section from './Components/Section'
+import Section from "./Components/Section"
 import Section2 from './Components/Section2'
-import Section3 from './Section3'
-import Footer from './Footer'
+import Section3 from './Components/Section3'
+import Footer from './Components/Footer'
 
 export default function App() {
   return (
-    <>
-    <Navbar/>
-   <Section/>
-   <Section2/>
-   <Section3/>
-   <Footer/>
-   </>
+    <div>
+      <Navbar/>
+      <Section/>
+      <Section2/>
+      <Section3/>
+      <Footer/>
+    </div>
   )
 }

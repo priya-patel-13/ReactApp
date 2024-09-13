58edf159a0daae6b2e62e3a9eebb81dc8c239cd7
priya-patel-13 <ppiyu4487@gmail.com>
2024-09-13 13:08:41 +0530

    can,uncantrolled

---
+++ Cantrolledcom/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cantrolled from './Component/Cantrolled'
+import Uncantrolled from './Component/Uncantrolled'
+
+export default function App() {
+  return (
+    <div>
+      <Cantrolled/>
+      <Uncantrolled/>
+    </div>
+  )
+}
